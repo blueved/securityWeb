e761d002db1f8d126161ae9d50bab2034d8ef224
blueved <ferre@blueved.com>
2016-02-29 20:21:37 -0600

    complete move to time range directive

--- app/components/dateRange/DateRangeDirective.js
+++ app/components/dateRange/DateRangeDirective.js
@@ -2,11 +2,149 @@ app.directive( "dateRange",  function(){
     return {
         restrict: 'EA',
         scope:{
-            dateFrom:'@',
-            dateTo :'@'
+            dtFrom: '=',
+            dtTo :  '='
         },
-        templateUrl: '/public/app/components/dateRange/dateRangeTemplate.html'
         
+        controller: function($scope){
+            var self = this;
+            
+            self.setRange = function(item){
+                switch (item){
+                    case 'Previous Month':
+                        self.setPreviousMonth();
+                        break;
+                    case 'Previous Week':
+                        self.setPreviousWeek();
+                        break;
+                    case 'Today':
+                        self.dtFrom = new Date();
+                        self.dtTo = new Date();
+                        break;
+                    case 'Current Week':
+                        self.setCurrentWeek();
+                        break;
+                    case 'Current Month':
+                        self.setCurrentMontMonth();
+                        break;
+                }
+            };
+            self.clear = function() {
+                self.dtFrom = null;
+                self.dtTo = null;
+            };
+            self.setPreviousWeek = function(){
+                  var from =  moment().subtract(1, 'weeks').startOf('isoWeek');
+                  var to  = moment().subtract(1, 'weeks').endOf('isoWeek');
+                  self.dtFrom = new Date(from);
+                  self.dtTo = new Date(to);
+            };
+            self.setPreviousMonth = function(){
+                  var from =  moment().subtract(1,'months').startOf('month').format('YYYY-MM-DD');
+                  var to  = moment().subtract(1,'months').endOf('month').format('YYYY-MM-DD');
+                  self.dtFrom = new Date(from);
+                  self.dtTo = new Date(to);
+              };
+            self.setCurrentWeek = function(){
+                    var from =  moment().startOf('week').format('YYYY-MM-DD');
+                    var to  = moment().format('YYYY-MM-DD');
+                    self.dtFrom = new Date(from);
+                    self.dtTo = new Date(to);
+            };
+            self.setCurrentMontMonth = function(){
+                  var from =  moment().startOf('month').format('YYYY-MM-DD');
+                  var to  = moment().format('YYYY-MM-DD');
+                  self.dtFrom = new Date(from);
+                  self.dtTo = new Date(to);
+              };
+            
+         // Disable weekend selection
+          self.disabled = function(date, mode) {
+            return mode === 'day' && (date.getDay() === 0 || date.getDay() === 6);
+          };
+
+          self.toggleMin = function() {
+            self.minDate = self.minDate ? null : new Date();
+          };
+
+          self.toggleMin();
+          self.maxDate = new Date(2020, 5, 22);
+
+          self.open1 = function() {
+            self.popup1.opened = true;
+          };
+
+          self.open2 = function() {
+            self.popup2.opened = true;
+          };
+
+         /* self.setDate = function(year, month, day) {
+            self.dt = new Date(year, month, day);
+          };         
+          var tomorrow = new Date();
+          tomorrow.setDate(tomorrow.getDate() + 1);
+          var afterTomorrow = new Date();
+          afterTomorrow.setDate(tomorrow.getDate() + 1);
+          self.events =
+            [
+              {
+                date: tomorrow,
+                status: 'full'
+              },
+              {
+                date: afterTomorrow,
+                status: 'partially'
+              }
+            ];
+
+          self.getDayClass = function(date, mode) {
+            if (mode === 'day') {
+              var dayToCheck = new Date(date).setHours(0,0,0,0);
+
+              for (var i = 0; i < self.events.length; i++) {
+                var currentDay = new Date(self.events[i].date).setHours(0,0,0,0);
+
+                if (dayToCheck === currentDay) {
+                  return self.events[i].status;
+                }
+              }
+            }
+
+            return '';
+          };*/
+
+          
+            
+            self.init = function(){
+                self.dateRange = "Today";
+                self.dtFrom = new Date();
+                self.dtTo   = new Date();
+                self.choicesDate = ['Previous Month', 'Previous Week', 'Today', 'Current Week', 'Current Month' ];
+                self.dateOptions = {
+                    formatYear: 'yy',
+                    startingDay: 1
+                  };
+
+                self.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+                self.format = self.formats[0];
+                self.altInputFormats = ['M!/d!/yyyy'];
+
+                self.popup1 = {   opened: false    };
+
+                self.popup2 = {   opened: false    };
+                console.log("Date range directive initialized");
+            };
+            self.init();
+        },
+        controllerAs:'vm',
+        templateUrl: '/public/app/components/dateRange/dateRangeTemplate.html',
+        link: function ($scope, element, attrs) { 
+            
+        } 
     };
+    
+
+          
+    
     
 });
